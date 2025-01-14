6d9eb0337073cf5f9f7750d2aecf391dff6b3536
Tomasz N <troll.yuck@gmail.com>
2025-01-14 19:12:03 +0100

    Update

--- exercise1/src/day-of-year.cpp
+++ exercise1/src/day-of-year.cpp
@@ -1,29 +1,20 @@
 #include "day-of-year.hpp"
+#include <array>
 
 int dayOfYear(int month, int dayOfMonth, int year) {
-    if (month == 2) {
-        dayOfMonth += 31;
-    } else if (month == 3) {
-        dayOfMonth += 59;
-    } else if (month == 4) {
-        dayOfMonth += 90;
-    } else if (month == 5) {
-        dayOfMonth += 31 + 28 + 31 + 30;
-    } else if (month == 6) {
-        dayOfMonth += 31 + 28 + 31 + 30 + 31;
-    } else if (month == 7) {
-        dayOfMonth += 31 + 28 + 31 + 30 + 31 + 30;
-    } else if (month == 8) {
-        dayOfMonth += 31 + 28 + 31 + 30 + 31 + 30 + 31;
-    } else if (month == 9) {
-        dayOfMonth += 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31;
-    } else if (month == 10) {
-        dayOfMonth += 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30;
-    } else if (month == 11) {
-        dayOfMonth += 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31;
-    } else if (month == 12) {
-        dayOfMonth += 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 31;
+    std::array<int, 12> daysInMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+    // Check for leap year
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+        daysInMonth[1] = 29;
     }
-    return dayOfMonth;
+
+    // Calculate the day of the year
+    int dayOfYear = dayOfMonth;
+    for (int i = 0; i < month - 1; ++i) {
+        dayOfYear += daysInMonth[i];
+    }
+
+    return dayOfYear;
 }
 
--- exercise1/ut/day-of-year-tests.cpp
+++ exercise1/ut/day-of-year-tests.cpp
@@ -1,6 +1,8 @@
 #include "gtest/gtest.h"
 #include "day-of-year.hpp"
 
+#include <iostream>
+
 struct DayOfYearTestSuite {};
 
 TEST(DayOfYearTestSuite, dummyTest)
@@ -13,3 +15,12 @@ TEST(DayOfYearTestSuite, January1stIsFitstDayOfYear)
   ASSERT_EQ(dayOfYear(1, 1, 2020), 1);
 }
 
+TEST(DayOfYearTestSuite, 2012LeapYear)
+{
+  ASSERT_EQ(dayOfYear(12, 31, 2012), 366);
+}
+
+TEST(DayOfYearTestSuite, 2013NonLeapYear)
+{
+  ASSERT_EQ(dayOfYear(12, 31, 2013), 365);
+}
